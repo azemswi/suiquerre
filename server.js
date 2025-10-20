0673401ab046489b107aa1a5549acaffe7755c9e
azemswi <appzem@gmail.com>
2025-10-20 10:32:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const SwissQRBill = require("swissqrbill");
+const { QRBill } = require("qrbill");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/api/generate", (req, res) => {
-  const bill = req.body;
-
+app.post("/api/generate", async (req, res) => {
   try {
-    const svg = SwissQRBill.generateSVG(bill);
+    const bill = new QRBill({
+      account: req.body.account,
+      amount: req.body.amount,
+      currency: req.body.currency || "CHF",
+      creditor: {
+        name: req.body.creditor.name,
+        address: req.body.creditor.address,
+        zip: req.body.creditor.zip,
+        city: req.body.creditor.city,
+        country: req.body.creditor.country
+      },
+      reference: req.body.reference,
+      additionalInfo: req.body.additionalInfo
+    });
+
+    const svg = bill.toSVG();
     res.setHeader("Content-Type", "image/svg+xml");
     res.status(200).send(svg);
   } catch (err) {
@@ -18,10 +31,11 @@ app.post("/api/generate", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Suiquerre SwissQRBill API is running");
+  res.send("Suiquerre QR Swiss API is running");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
